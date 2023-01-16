e27bd6ec6a7f4ff30ec0d40b8176d8deee92b7a9
Ivane Tvildiani <ivane.tvildiani.1@btu.edu.ge>
2023-01-16 22:44:00 +0400

    Everything in 1 commit
    sorry for that

---
+++ Javascript homework/script.js
@@ -0,0 +1,66 @@
+
+// _____________1_______________
+
+let user = 'Ivane';
+let age = 23;
+let message = 'Hello World';
+// _____________2_______________
+
+let x = 5
+let y = 10
+
+let matematika = x + y 
+console.log(matematika);
+// __________________
+
+let users = 100
+let dayoff = 15
+
+let weekend = users - dayoff
+console.log(weekend);
+
+// _________________
+
+let ye = 10
+let ay = 10
+
+let sup = ye * ay
+console.log(sup);
+
+// _________________
+
+let n = 100
+let m = 5 
+
+let OG = n / m
+console.log(OG);
+
+// _____________3_4_______________
+
+let length = parseFloat(prompt("შეიყვანეთ სიგრძე: "));
+let width = parseFloat(prompt("შეიყვანეთ სიგანე: "))
+ 
+let area = length * width;
+ 
+console.log(`ფართობი არის ${area}`);
+
+// _________________
+
+let sigrdze = 5;
+let sigane = 7; 
+
+let part = sigrdze * sigane;
+
+console.log(part);
+
+// _____________5_______________
+
+const PI = Math.PI;
+let radius = 7;
+let circle;
+
+radius = Number(radius);
+
+circle = 2 * PI * radius;
+
+console.log(circle);
